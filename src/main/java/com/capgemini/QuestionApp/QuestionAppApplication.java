37456f2d25d7b1cb5da75cdfaa4e38c6cf893465
Dan Lee <daniel.a.lee@capgemini.com>
2018-06-13 12:33:56 -0500

    first commit REDO

---
+++ src/main/java/com/capgemini/QuestionApp/QuestionAppApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.QuestionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/capgemini/QuestionApp/models/User.java
@@ -0,0 +1,50 @@
+package com.capgemini.QuestionApp.models;
+
+
+public class User {
+	
+	private Long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
